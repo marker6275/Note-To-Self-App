b3a55ef8564fab5160602f4ea4a7c4674c5c6ce6
ecz2515 <chenevan0310@gmail.com>
2022-12-23 10:46:55 +0800

    stylise congrats page

--- screens/Congrats.tsx
+++ screens/Congrats.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from '../components/Themed';
+import { StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Image } from 'react-native';
 
-// Will make this look better later
-export default function Congrats() {
-    return (
-        <Text>
-            Congrats!
-        </Text>
-    )
+import { Text, View } from '../components/Themed';
+import { RootStackScreenProps } from '../types';
+
+export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('../images/flower.png')} />
+      <Text style={styles.title}>Congrats! We're proud of you.</Text>
+      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
+        <Text style={styles.linkText}>Return to home screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'lightgreen',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+  image: {
+    flex: 0.5,
+    width: 100,
+    resizeMode: 'contain',
+  }
+});
