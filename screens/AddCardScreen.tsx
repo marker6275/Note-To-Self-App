4d5a40a886f169e84a992da6bc444ff082b70b43
ecz2515 <chenevan0310@gmail.com>
2022-12-25 23:22:42 +0800

    add functionality to add card screen

--- screens/AddCardScreen.tsx
+++ screens/AddCardScreen.tsx
@@ -1,7 +1,16 @@
-import { TextInput } from 'react-native';
+import { TextInput, ScrollView } from 'react-native';
 import { View, Text } from '../components/Themed';
 import { useState } from 'react';
 import { RootStackScreenProps } from '../types';
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet, Dimensions } from 'react-native';
+import { Menu } from 'react-native-paper';
+import EditScreenInfo from '../components/EditScreenInfo';
+import Button from '../components/Button';
+import NotFoundScreen from './NotFoundScreen';
+import { useLinkProps, useNavigation } from '@react-navigation/native'
+import TabOneScreen from './Homescreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function AddCard({ navigation }: RootStackScreenProps<'AddCard'>) {
     const [text, setText] = useState('');
@@ -14,15 +23,81 @@ export default function AddCard({ navigation }: RootStackScreenProps<'AddCard'>)
         </View>
     }
     return (
-        <View>
+        <ScrollView>
+            
+            <Text style={styles.title}>What would you like to add?</Text>
             <TextInput
-                style={{height: 40, color: 'white'}}
-                placeholder="Type here to add text!"
+                style={{height: 35, 
+                    color: 'white', 
+                    backgroundColor: 'white',
+                    marginHorizontal: 20,
+                    borderRadius: 15,}}
+                placeholder="Search"
                 onChangeText={newText => setText(newText)}
                 defaultValue={text}
+                textAlign="center"
                 clearButtonMode='while-editing'
                 onSubmitEditing={showText}
             />
-        </View>
+            <View style={styles.separator} lightColor="transparent" darkColor="transparent"/>
+
+            <Text style={styles.emphasis}>Categories that haven't been added:</Text>
+            {/* FIGURE OUT HOW TO DO THIS */}
+            <Button title="FIGURE THIS OUT" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="FIGURE THIS OUT" onPress={() => navigation.navigate('NotFound')}/>
+
+            {/* <View style={styles.separator}/> */}
+            <Text style={styles.emphasis}>All Categories:</Text>
+
+                
+            <Button title="Food" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Music" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Video" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Activity" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Quote" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Meditation" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Journal Entry" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="People" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Pets" onPress={() => navigation.navigate('NotFound')}/>
+            <Button title="Pictures" onPress={() => navigation.navigate('NotFound')}/>
+
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'ghostwhite',
+      justifyContent: 'center',
+      alignContent: 'center'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        marginVertical: 20,
+    },
+      emphasis: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingLeft: '5%',
+        marginBottom: 15,
+    },
+    button: {
+      width: Dimensions.get('window').width,
+    },
+    item: {
+      fontSize: 16,
+      paddingLeft: '15%',
+      paddingVertical: 30,
+      borderBottomColor: 'black',
+      borderBottomWidth: 1,
+      color: 'black'
+    },
+    separator: {
+        marginVertical: 20,
+        height: 1,
+        width: '80%',
+    }
+  });
