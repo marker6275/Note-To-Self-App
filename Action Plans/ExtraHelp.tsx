5997cceed948ac6902f809336eb77924b1b0c383
ecz2515 <chenevan0310@gmail.com>
2022-12-27 22:42:26 +0800

    Implement find nearest emergency room

--- Action Plans/ExtraHelp.tsx
+++ Action Plans/ExtraHelp.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, TouchableOpacity, Button } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -11,13 +11,19 @@ export default function ExtraHelp({ navigation }: RootStackScreenProps<'ExtraHel
     <View style={styles.container}>
 
       <Text style={styles.title}>Crisis Hotline</Text>
-      <Text style={styles.text}>Text "HOME" to <Text style={styles.phone} onPress={()=>{Linking.openURL('tel:8329458288');}}>741741</Text> NEED TO FIX</Text>
+      <Text style={styles.text}>Text "HOME" to <Text style={styles.phone} onPress={()=>{Linking.openURL('tel:+1(832)945-8288');}}>741741</Text> NEED TO FIX</Text>
 
       <Text style={styles.separator}/>
       <Text style={styles.title}>National Suicide Prevention Lifeline</Text>
       {/* NEED TO CHANGE PHONE NUMBER LATER */}
       <Text>Don't press this button</Text>
-      <Text style={styles.text}>Call <Text style={styles.phone} onPress={()=>{Linking.openURL('tel:8329458288');}}>988</Text> NEED TO FIX</Text>
+      <Text style={styles.text}>Call <Text style={styles.phone} onPress={()=>{Linking.openURL('tel:+1(832)945-8288');}}>988</Text> NEED TO FIX</Text>
+
+      <Text style={styles.separator}/>
+      <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('http://maps.apple.com/?q=Emergency+Room')}>
+        <Text style={styles.title}>Find the nearest emergency room</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -44,5 +50,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#7BB0E5',
     fontWeight: 'bold'
-  }
+  },
+  button: {
+    backgroundColor: '#FFBB98',
+    padding: 10,
+    borderRadius: 10,
+},
 });
