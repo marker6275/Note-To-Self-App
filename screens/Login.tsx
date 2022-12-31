a9be5f85938ad0b12680de2f9988f6fb693786b8
ecz2515 <chenevan0310@gmail.com>
2022-12-31 20:57:46 +0800

    stylise login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from '../components/Themed';
-import { TextInput } from 'react-native';
+import { TextInput, StyleSheet, Image } from 'react-native';
 import { useState } from 'react';
 
 export default function Login() {
@@ -21,38 +21,44 @@ export default function Login() {
         </View>
     }
     return (
-        <View>
-        <Text>LOGIN SCREEN</Text>
-        <TextInput
-            style={{height: 50, 
-                color: 'black', 
-                backgroundColor: 'white',
-                // marginHorizontal: 20,
-            }}
-            placeholder="Username"
-            placeholderTextColor={'blue'}
-            onChangeText={(newText) => setText(newText)}
-            defaultValue={text}
-            // textAlign="center"
-            clearButtonMode='while-editing'
-            // onSubmitEditing={showText}
-        />
-        <TextInput
-            style={{height: 50, 
-                color: 'black', 
-                backgroundColor: 'white',
-                // marginHorizontal: 20,
-                marginTop: 25
-            }}
-            placeholder="Password"
-            placeholderTextColor={'blue'}
-            onChangeText={newText => setPassword(newText)}
-            defaultValue={password}
-            // textAlign="center"
-            multiline={false}
-            clearButtonMode='while-editing'
-            // onSubmitEditing={showPassword}
-        />
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../images/flower.png')} />
+            <Text style={styles.title}>Hello.</Text>
+            <Text style={styles.subtitle}>Welcome to Note To Self.</Text>
+            <TextInput
+                style={{height: 50, 
+                    color: 'black', 
+                    backgroundColor: 'white',
+                    padding: 10,
+                    borderRadius: 15,
+                    // marginHorizontal: 20,
+                }}
+                placeholder="Username"
+                placeholderTextColor={'grey'}
+                onChangeText={(newText) => setText(newText)}
+                defaultValue={text}
+                // textAlign="center"
+                clearButtonMode='while-editing'
+                // onSubmitEditing={showText}
+            />
+            <TextInput
+                style={{height: 50, 
+                    color: 'black', 
+                    backgroundColor: 'white',
+                    // marginHorizontal: 20,
+                    marginTop: 25,
+                    padding: 10,
+                    borderRadius: 15,
+                }}
+                placeholder="Password"
+                placeholderTextColor={'grey'}
+                onChangeText={newText => setPassword(newText)}
+                defaultValue={password}
+                // textAlign="center"
+                multiline={false}
+                clearButtonMode='while-editing'
+                // onSubmitEditing={showPassword}
+            />
         <Text>
             {text}
             {password}
@@ -60,3 +66,39 @@ export default function Login() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    //   alignItems: 'center',
+      justifyContent: 'center',
+      padding: 30,
+      backgroundColor: 'lightpink',
+    },
+    title: {
+      fontSize: 50,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      alignSelf: 'center',
+    },
+    subtitle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        alignSelf: 'center',
+      },
+    link: {
+      marginTop: 15,
+      paddingVertical: 15,
+    },
+    linkText: {
+      fontSize: 14,
+      color: '#2e78b7',
+    },
+    image: {
+      flex: 0.5,
+      width: 75,
+      resizeMode: 'contain',
+      alignSelf: 'center',
+    },
+  });
